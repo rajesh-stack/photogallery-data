5c875e63f835095150f18941ae317a4e95a01e1f
Rajesh panchal <rajesh.panchal@green-apex.com>
2022-05-27 11:38:43 +0530

    db changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('photos.json'); // <== Will be created later
+const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 2000; // <== You can change the port
+const port = process.env.PORT || 3400; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
